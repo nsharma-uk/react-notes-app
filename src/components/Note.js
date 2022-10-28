9f3efa2cabe54d98ae3d8097ad175b4e66b94623
nsharma-uk <n.sharmauk711@gmail.com>
2022-10-28 16:07:19 +0100

    basic components and page set up

---
+++ src/components/Note.js
@@ -0,0 +1,13 @@
+const Note = () => {
+  return (
+    <div className="note">
+      <h1>Hello this is a new note</h1>
+      <div className="note-footer">
+        <p>28/10/2022</p>
+      </div>
+      <p>Delete</p>
+    </div>
+  );
+};
+
+export default Note;
---
+++ src/components/NotesPage.js
@@ -0,0 +1,12 @@
+import NotesList from "./NotesList";
+
+const NotesPage = () => {
+  return (
+    <div className="container">
+      {" "}
+      <NotesList />
+    </div>
+  );
+};
+
+export default NotesPage;
