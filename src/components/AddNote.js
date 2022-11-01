39562e364ab729df18d1ef9cd6848c149fbb44bf
nsharma-uk <n.sharmauk711@gmail.com>
2022-11-01 12:12:11 +0000

    handle change on input into text area

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,9 +1,20 @@
+import { useState } from "react";
 import { RiSave2Line } from "react-icons/ri";
 
 const AddNote = () => {
+  const [addText, setAddText] = useState("");
+
+  const handleChange = (e) => {
+    console.log(e.target.value);
+  };
   return (
     <div className="note new">
-      <textarea rows="8" cols="10" placeholder="Type here to add a note" />
+      <textarea
+        rows="8"
+        cols="10"
+        placeholder="Type here to add a note"
+        onChange={handleChange}
+      />
 
       <div className="note-footer">
         <p className="note-footer-text">200 characters remaining</p>
