e94ea90ba50d4aa8eaed50444007bcf322b2d8fd
nsharma-uk <n.sharmauk711@gmail.com>
2022-11-01 17:09:27 +0000

    saving new notes with date and id

--- src/App.js
+++ src/App.js
@@ -1,34 +1,51 @@
 import { useState } from "react";
-import { v5 as uuid } from "uuid";
-
+import { v4 as uuid } from "uuid";
 import NotesList from "./components/NotesList";
+
+const unique_id = uuid();
+const small_id = unique_id.slice(0, 8);
 
 export const App = () => {
   const [notes, setNote] = useState([
     {
-      // id: uuid(),
+      id: small_id,
       text: "This is the first note",
       date: "12/10/22022",
     },
     {
-      // id: uuid(),
+      id: small_id,
       text: "This is the 2nd note",
       date: "15/10/22022",
     },
     {
-      // id: uuid(),
+      id: small_id,
       text: "This is the 3rd note",
       date: "16/10/22022",
     },
     {
-      // id: uuid(),
+      id: small_id,
       text: "This is the 4th note",
       date: "30/10/22022",
     },
   ]);
+
+  const addNote = (text) => {
+    console.log(text);
+    const date = new Date();
+    const newNote = {
+      id: small_id,
+      text: text,
+      date: date.toLocaleDateString(),
+    };
+
+    console.log(newNote);
+    const newNotes = [...notes, newNote];
+    setNote(newNotes);
+  };
+
   return (
     <div className="container">
-      <NotesList notes={notes} />
+      <NotesList notes={notes} handleAddNote={addNote} />
     </div>
   );
 };
--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -1,24 +1,31 @@
 import { useState } from "react";
 import { RiSave2Line } from "react-icons/ri";
 
-const AddNote = () => {
+const AddNote = ({ handleAddNote }) => {
   const [addText, setAddText] = useState("");
 
   const handleChange = (e) => {
     console.log(e.target.value);
+    setAddText(e.target.value);
   };
+
+  const handleSaveClick = () => {
+    handleAddNote(addText);
+  };
+
   return (
     <div className="note new">
       <textarea
         rows="8"
         cols="10"
-        placeholder="Type here to add a note"
+        placeholder="Type here to add a note..."
+        value={addText}
         onChange={handleChange}
       />
 
       <div className="note-footer">
         <p className="note-footer-text">200 characters remaining</p>
-        <RiSave2Line />
+        <RiSave2Line onClick={handleSaveClick} />
       </div>
     </div>
   );
