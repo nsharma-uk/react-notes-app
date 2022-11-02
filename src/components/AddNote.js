9ad8a12321fb382e785453b8c1287444efda458f
nsharma-uk <n.sharmauk711@gmail.com>
2022-11-02 13:33:07 +0000

    error message (wip) & character remaining on note

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -3,14 +3,25 @@ import { RiSave2Line } from "react-icons/ri";
 
 const AddNote = ({ handleAddNote }) => {
   const [addText, setAddText] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
 
+  const characterLimit = 300;
+
+  //handle text input
   const handleChange = (e) => {
     console.log(e.target.value);
     setAddText(e.target.value);
   };
 
+  //handle save
   const handleSaveClick = () => {
-    handleAddNote(addText);
+    if (addText.trim().length > 0) {
+      handleAddNote(addText);
+      setAddText("");
+    } else {
+      setErrorMsg("Please type something to save your note");
+      console.log(setErrorMsg);
+    }
   };
 
   return (
@@ -24,8 +35,11 @@ const AddNote = ({ handleAddNote }) => {
       />
 
       <div className="note-footer">
-        <p className="note-footer-text">200 characters remaining</p>
+        <p className="note-footer-text">
+          {characterLimit - addText.length} remaining
+        </p>
         <RiSave2Line onClick={handleSaveClick} />
+        {errorMsg && <p className="error"> {errorMsg} </p>}
       </div>
     </div>
   );
